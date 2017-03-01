5964750f15f2c26e020a13910a2f49a60a532aa2
KenrodRoberts <kenrodroberts@gmail.com>
2017-03-01 04:28:52 -0400

    Final Update to Lunch Checker

--- solutions/module1-solution/app.js
+++ solutions/module1-solution/app.js
@@ -10,21 +10,23 @@ function LunchCheckerController($scope){
     
 	$scope.checkQuantity = function () {
 		$scope.message = "";
-		console.log("Error: "+$scope.message);
-		console.log("List: "+$scope.menuList);
+		$scope.messageStyle="";
+
 		if ($scope.menuList){			
 			if ($scope.menuList.split(',').length < 4) {
 				$scope.message = "Enjoy!";
+				$scope.messageStyle="greenMessage";
 
 			} else {
 				$scope.message = "Too Much!";
+				$scope.messageStyle="greenMessage";
 			};
 		}else{
 			$scope.message = "Please enter data first";
+			$scope.messageStyle="redMessage";
 		};
 		
 	};
 }
-
 
 })();
